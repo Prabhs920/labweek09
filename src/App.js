2b086b45df9b88e225a360653e3936d584596288
Prabhs920 <prabhnoors920@gmail.com>
2024-11-03 21:55:37 -0500

    Initial commit for labweek09

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import logo from './logo.svg'; // The default React logo that comes with Create React App
+import './App.css'; // This is for styling
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <img src={logo} className="logo" alt="React Logo" />
+        <h1>Welcome to Fullstack Development - I</h1>
+        <h2>React JS Programming Week09 Lab exercise</h2>
+        <p>Your Student ID: 101415063</p>
+        <p>Your Name: Prabhnoor Singh</p>
+        <p>George Brown College, Toronto</p>
+      </div>
     </div>
   );
 }
